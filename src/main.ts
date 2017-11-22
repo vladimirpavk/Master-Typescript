3cd39305e6dc53440008dcec516b3ffb071bbee5
Vladimir Pavković <vladimirpavk@gmail.com>
2017-11-22 14:38:09 +0100

    Vezbanje

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 'use strict'
 
 function probaFunc(nekiObj: {
+    ime?: string,
     label: string
 }){
-    console.log(nekiObj.label);
+    if(!nekiObj.ime){
+        console.log(nekiObj.label);
+    }
+    else{
+        console.log(nekiObj.ime+" - "+nekiObj.label);
+    }
 }
 
 
@@ -11,3 +17,30 @@ probaFunc({
     ime: "vladimir",
     label: "neka labela"
 });
+
+let broj:string = "3";
+
+console.log(parseInt(broj)+3);
+
+console.log(0b10);
+
+let niz=[];
+niz.push(1,2,3,4,5,6,7,8,9);
+
+niz.forEach(
+    (val, ind)=>
+    {
+        console.log(val, " - "+ind);
+    }
+);
+
+
+let xObj={
+    ime: 'Vladimir',
+    prezime: 'Pavković',
+    zena: 'Nataša'
+};
+
+for(let xProp of niz){
+    console.log(xProp);
+}
